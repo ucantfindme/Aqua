e2842b4f16ffcda4335c92e970ebacb2e648483f
Gopi4330 <vasanthagopikrishna@gmail.com>
2022-03-22 21:54:42 +0530

    added class files(person,teacher,student,admin)

---
+++ Teacher.py
@@ -0,0 +1,21 @@
+from .Person import Person
+class Teacher(Person):
+    def __init__(self,name,dob,ph,email,teacherId,department,courses_taught,password):
+        super().__init__(name,dob,ph,email)
+        self.teacherId=teacherId
+        self.department=department
+        self.courses_taught=courses_taught
+        self.password=password
+    def scheduleMeeting(self,course,date):
+        pass
+    def uploadAttendance(self,meeting):
+        pass
+    def viewScheduleMeetings(self):
+        pass
+    def viewMeetingAttendance(self,meeting):
+        pass
+    def viewOverallAttendance(self,course):
+        pass
+    def viewProfile(self):
+        pass
+    
