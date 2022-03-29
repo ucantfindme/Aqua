25b2f78eafd411ac19a5fb5e2fa052dae842f734
Deepti Hada <53363509+deeptiiii@users.noreply.github.com>
2022-03-29 22:04:39 +0530

    Update teacher.py

--- src/GUI/teacher.py
+++ src/GUI/teacher.py
@@ -10,19 +10,21 @@ import random
 from datetime import datetime
 from tkinter import filedialog
 from Classes.Welcome import login as logfun, forgotPassword as forpass, logout as lgo
-#from home import welcome
 
+
+# Teacher class
 class teacher():
     def __init__(self):
         pass
 
+    # Home func
     def refresh(self,h):
         h.destroy()
         teacher().home()
         pass
-    # welcome frame
+    # Welcome frame
     def home(self):
-        # home
+        # Home window
         h=Tk()
         h.title("AMS Teacher")
         h.configure(background='purple')
@@ -42,7 +44,7 @@ class teacher():
         # Registration button
         reg = Button(f1,text="Register",bg='orange',relief='flat',underline=0,font=('roboto',12,'bold'))
         reg.place(x=90,y=380)
-        #close button
+        # Close button
         cls = Button(f1,text="Logout",bg='orange',relief='flat',underline=0,command=lambda:lgo(h),font=('roboto',12,'bold'))
         cls.place(x=90,y=500)
         
@@ -56,6 +58,7 @@ class teacher():
         # Frame 3
         f3 = Frame(h,width=980,height=530,bg='lightblue')
         f3.place(x=210,y=160)
+        # Loading image
         path=os.path.abspath('.')+"/src/GUI/Aqua.png"
         img=Image.open(path)
         img = img.resize((980, 530), Image. ANTIALIAS)
